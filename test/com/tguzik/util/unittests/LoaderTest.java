8afc6371ef29f91b7ca6ffe17a098719815ab5e7
Tomasz Guzik <tomek@tguzik.com>
2014-01-25 18:47:30 +0100

    Split TestUtils into Loader and Normalize, added SettableHashCode, added unit tests for the first two.

---
+++ test/com/tguzik/util/unittests/LoaderTest.java
@@ -0,0 +1,26 @@
+package com.tguzik.util.unittests;
+
+import static com.tguzik.util.unittests.Loader.loadFile;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * @author Tomasz Guzik <tomek@tguzik.com>
+ */
+public class LoaderTest
+{
+    // TODO: Write more unit tests
+
+    @Test
+    public void testLoadFile( ) throws IOException {
+        String fileContents = loadFile( "test", getClass(), "files", "test-file.txt" );
+
+        assertNotNull( fileContents );
+        assertEquals( 20, fileContents.length() );
+        assertEquals( "Test file contents\n\n", fileContents );
+    }
+}
