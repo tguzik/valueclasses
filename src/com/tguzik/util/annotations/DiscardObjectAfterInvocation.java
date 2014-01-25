7d6e340f7515ce45cd99f198f29487b9b4980bc2
Tomasz Guzik <tomek@tguzik.com>
2014-01-25 19:36:25 +0100

    Added @DiscardObjectAfterInvocation

---
+++ src/com/tguzik/util/annotations/DiscardObjectAfterInvocation.java
@@ -0,0 +1,19 @@
+package com.tguzik.util.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * TODO: Documentation
+ * 
+ * 
+ * @author Tomasz Guzik <tomek@tguzik.com>
+ * @since 0.1
+ */
+@Target( ElementType.METHOD )
+@Retention( RetentionPolicy.CLASS )
+public @interface DiscardObjectAfterInvocation {
+
+}
