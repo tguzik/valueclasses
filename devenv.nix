7947ab90cc49a9ce1a43074c852fa5853b990b36
Tomasz Guzik <github@tguzik.com>
2025-11-16 12:25:52 +0100

    Enable git-hooks integrations in devenv

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,13 @@
-{ pkgs, lib, config, inputs, ... }:
 {
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+{
+  # See full reference at https://devenv.sh/reference/options/
+
   # https://devenv.sh/languages/
   languages = {
     java = {
@@ -17,10 +25,35 @@
     pkgs.git
     pkgs.curl
     pkgs.go-task
-
-    # Additional linters
-    pkgs.actionlint
   ];
+
+  git-hooks.hooks = {
+    actionlint.enable = true;
+    gitlint.enable = true;
+    markdownlint = {
+      enable = true;
+      settings.configuration = {
+        MD013 = {
+          line_length = 120;
+        };
+        MD033 = false;
+        MD034 = false;
+      };
+    };
+    nixfmt.enable = true;
+    no-commit-to-branch.enable = true;
+    shellcheck.enable = true;
+    trufflehog.enable = true;
+    yamllint = {
+      enable = true;
+      settings.configuration = ''
+        extends: relaxed
+        rules:
+          line-length:
+            max: 150
+      '';
+    };
+  };
 
   enterShell = ''
     # Do nothing
@@ -29,9 +62,4 @@
   enterTest = ''
     # Do nothing
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
